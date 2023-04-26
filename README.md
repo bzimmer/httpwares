b55c03434f4ae5d5b1e277d9eb8c6dfc2047bc14
Brian Zimmer <bzimmer@ziclix.com>
2023-04-26 19:45:07 +0200

    remove unnecessary transports

--- README.md
+++ README.md
@@ -3,11 +3,11 @@
 [![build](https://github.com/bzimmer/httpwares/actions/workflows/build.yaml/badge.svg?branch=main)](https://github.com/bzimmer/httpwares/actions/workflows/build.yaml)
 [![codecov](https://codecov.io/gh/bzimmer/httpwares/branch/master/graph/badge.svg?token=JBACLW92NN)](https://codecov.io/gh/bzimmer/httpwares)
 
-A collection of useful `http.RoundTripper`s:
+A collection of useful `http.RoundTripper` implementations:
 
-* `VerboseTransport` wraps an instance of a `http.RoundTripper` to write the `http.Request` and `http.Response`
-* `TestDataTransport` enables returning the contents of local files to facilitate unit testing
-* `SleepingTransport` enables testing clients which need to simulate latency in responses
-* `RateLimitTransport` enables rate limit client requests by using a `golang.org/x/time/rate/Limiter` instance
+* `VerboseTransport` wraps an instance of a `http.RoundTripper` to write the
+  `http.Request` and `http.Response`
+* `RateLimitTransport` enables rate limit client requests by using a
+  `golang.org/x/time/rate/Limiter` instance
 
-See the test cases for how to use each of the `http.RoundTripper`s
+See the test cases for how to use each `http.RoundTripper`
--- ratelimit_test.go
+++ ratelimit_test.go
@@ -3,6 +3,7 @@ package httpwares_test
 import (
 	"context"
 	"net/http"
+	"net/http/httptest"
 	"testing"
 	"time"
 
@@ -17,53 +18,53 @@ func TestRateLimit(t *testing.T) {
 	t.Parallel()
 	a := assert.New(t)
 
-	limiter := rate.NewLimiter(rate.Every(1*time.Minute), 1)
-	client := http.Client{
-		Transport: &httpwares.RateLimitTransport{
-			Limiter: limiter,
-			Transport: &httpwares.TestDataTransport{
-				Status:      http.StatusOK,
-				Filename:    "transport.json",
-				ContentType: "application/json",
-			}},
+	mux := http.NewServeMux()
+	mux.HandleFunc("/transport.json", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "testdata/transport.json")
+	})
+
+	tests := []struct {
+		name string
+		dur  time.Duration
+	}{
+		{
+			name: "zero",
+			dur:  0 * time.Millisecond,
+		},
+		{
+			name: "with a duration",
+			dur:  250 * time.Millisecond,
+		},
 	}
 
-	ctx := context.Background()
-	req, err := http.NewRequestWithContext(ctx, http.MethodGet, "http://example.com", nil)
-	a.NoError(err)
-	a.NotNil(req)
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			svr := httptest.NewServer(mux)
+			defer svr.Close()
 
-	res, err := client.Do(req)
-	a.NoError(err)
-	a.NotNil(res)
-	a.NoError(res.Body.Close())
+			limiter := rate.NewLimiter(rate.Every(1*time.Minute), 1)
+			client := http.Client{
+				Transport: &httpwares.RateLimitTransport{
+					Limiter: limiter,
+				},
+			}
 
-	ctx = context.Background()
-	ctx, cancel := context.WithTimeout(ctx, time.Millisecond*250)
-	defer cancel()
+			ctx := context.Background()
+			if tt.dur > (0 * time.Millisecond) {
+				var cancel func()
+				ctx, cancel = context.WithTimeout(ctx, time.Millisecond*250)
+				defer cancel()
+			}
 
-	req, err = http.NewRequestWithContext(ctx, http.MethodGet, "http://example.com", nil)
-	a.NoError(err)
-	a.NotNil(req)
+			req, err := http.NewRequestWithContext(ctx, http.MethodGet, svr.URL+"/transport.json", nil)
+			a.NoError(err)
+			a.NotNil(req)
 
-	// rate: Wait(n=1) would exceed context deadline
-	res, err = client.Do(req)
-	a.Error(err)
-	a.Nil(res)
-	if res != nil {
-		a.NoError(res.Body.Close())
+			res, err := client.Do(req)
+			a.NoError(err)
+			a.NotNil(res)
+			a.NoError(res.Body.Close())
+		})
 	}
-
-	client = http.Client{
-		Transport: &httpwares.RateLimitTransport{
-			Transport: &httpwares.TestDataTransport{
-				Status:      http.StatusOK,
-				Filename:    "transport.json",
-				ContentType: "application/json",
-			}},
-	}
-	res, err = client.Do(req)
-	a.NoError(err)
-	a.NotNil(res)
-	a.NoError(res.Body.Close())
 }
