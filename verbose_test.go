dca6dc239fc645df50ed6cd226127bb204bbb673
Brian Zimmer <bzimmer@ziclix.com>
2023-04-27 06:47:44 +0200

    lint

--- verbose_test.go
+++ verbose_test.go
@@ -8,8 +8,9 @@ import (
 	"net/http/httptest"
 	"testing"
 
-	"github.com/bzimmer/httpwares"
 	"github.com/stretchr/testify/assert"
+
+	"github.com/bzimmer/httpwares"
 )
 
 func TestVerboseTransport(t *testing.T) {
